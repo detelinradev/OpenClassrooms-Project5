08f7c57002e07673d09a4f41f0690cf51b52c304
Detelin Radev <clover88bg@yahoo.com>
2020-11-21 23:22:01 +0200

    Changed test configuration to run with spring extension and used spring boot test annotation to initiate full application run, added autowired and created PersonController class so the test fell for one reason returns 404 instead of 200 HttpStatus

--- src/main/java/com/openclassroom/safetynet/model/Person.java
+++ src/main/java/com/openclassroom/safetynet/model/Person.java
@@ -1,6 +1,20 @@
 package com.openclassroom.safetynet.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
-    public Person(String cron, String boyd, String s, String boston, String s1, String s2, String s3) {
-    }
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String zip;
+    private String phone;
+    private String email;
 }
--- src/test/java/com/openclassroom/safetynet/acceptanceTests/AcceptanceTests.java
+++ src/test/java/com/openclassroom/safetynet/acceptanceTests/AcceptanceTests.java
@@ -1,26 +1,46 @@
 package com.openclassroom.safetynet.acceptanceTests;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassroom.safetynet.controller.PersonController;
 import com.openclassroom.safetynet.model.Person;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class AcceptanceTests {
 
+    @Autowired
     private MockMvc mockMvc;
-    private Person person = new Person("Cron","Boyd","1000 Monroe St","Boston","15000","123-444-555","cronboyd@email.com");
+
+    @Autowired
+    private PersonController personController;
+
+    private Person person;
     private JacksonTester<Person> jsonPerson;
+
+    @BeforeEach
+    public void setUp() {
+        person = new Person("Cron", "Boyd", "1000 Monroe St", "Boston", "15000", "123-444-555", "cronboyd@email.com");
+        JacksonTester.initFields(this, new ObjectMapper());
+        mockMvc = MockMvcBuilders.standaloneSetup(personController).build();
+    }
 
     @Test
     public void passingPersonToEndpointPersonWithPostMethod_Should_createNewPerson_When_conditionsAreMet() throws Exception {
