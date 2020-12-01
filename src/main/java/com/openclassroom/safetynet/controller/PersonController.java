b691741f93b49b1ebaf91934aa03ec66d6d27033
Detelin Radev <clover88bg@yahoo.com>
2020-12-01 13:28:24 +0200

    Create method delete Person in PersonController

--- src/main/java/com/openclassroom/safetynet/controller/PersonController.java
+++ src/main/java/com/openclassroom/safetynet/controller/PersonController.java
@@ -3,29 +3,35 @@ package com.openclassroom.safetynet.controller;
 import com.openclassroom.safetynet.model.Person;
 import com.openclassroom.safetynet.service.contracts.PersonService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/person")
 public class PersonController {
 
     private final PersonService personService;
 
-    @PostMapping(value = "/person")
+    @PostMapping
     public ResponseEntity<Person> createPerson(@RequestBody Person person){
 
         return ResponseEntity.ok().body(personService.createPerson(person));
     }
 
-    @PutMapping(value = "/person")
+    @PutMapping
     public ResponseEntity<Person> updatePerson(@RequestBody Person person){
 
         return  ResponseEntity.ok().body(personService.updatePerson(person));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<HttpStatus> deletePerson(@RequestBody Person person){
+
+        personService.deletePerson(person);
+        return ResponseEntity.accepted().build();
     }
 
 }
